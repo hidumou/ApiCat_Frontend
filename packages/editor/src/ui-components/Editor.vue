d7b72789f86d97d26196fb6b10188650f95ebd1e
haner <haner.du@qq.com>
2022-09-13 17:07:50 +0800

    add：editor ready event

--- packages/editor/src/ui-components/Editor.vue
+++ packages/editor/src/ui-components/Editor.vue
@@ -1,68 +1,69 @@
 <template>
-    <div ref="editor" class="ac-editor"></div>
+  <div ref="editor" class="ac-editor"></div>
 </template>
 
 <script>
-    import { markRaw } from 'vue'
-    import AcEditor, { EDITOR_EVENTS } from '../editor'
+  import { markRaw } from 'vue'
+  import AcEditor, { EDITOR_EVENTS } from '../editor'
 
-    export default {
-        name: 'AcEditor',
-        emits: ['on-change'],
-        props: {
-            document: {
-                type: Object,
-                default: () => ({}),
-            },
+  export default {
+    name: 'AcEditor',
+    emits: ['on-change', 'on-ready'],
+    props: {
+      document: {
+        type: Object,
+        default: () => ({}),
+      },
 
-            readonly: {
-                type: Boolean,
-                default: false,
-            },
+      readonly: {
+        type: Boolean,
+        default: false,
+      },
 
-            options: {
-                type: Object,
-                default: () => ({}),
-            },
-        },
+      options: {
+        type: Object,
+        default: () => ({}),
+      },
+    },
 
-        watch: {
-            document: function () {
-                this.editor && this.editor.setContent(this.document)
-            },
-        },
+    watch: {
+      document: function () {
+        this.editor && this.editor.setContent(this.document)
+      },
+    },
 
-        data() {
-            return {
-                editor: null,
-            }
-        },
+    data() {
+      return {
+        editor: null,
+      }
+    },
 
-        methods: {
-            focus(isLast) {
-                this.editor.focus(isLast)
-            },
-        },
+    methods: {
+      focus(isLast) {
+        this.editor.focus(isLast)
+      },
+    },
 
-        mounted() {
-            this.editor = markRaw(
-                AcEditor.create(this.$refs.editor, {
-                    ...this.options,
-                    readonly: this.readonly,
-                    content: this.document,
-                })
-            )
+    mounted() {
+      this.editor = markRaw(
+        AcEditor.create(this.$refs.editor, {
+          ...this.options,
+          readonly: this.readonly,
+          content: this.document,
+        })
+      )
 
-            this.editor.on(EDITOR_EVENTS.Update, () => {
-                this.$emit('on-change', this.editor)
-            })
+      this.editor.on(EDITOR_EVENTS.Update, () => {
+        this.$emit('on-change', this.editor)
+      })
 
-            window.$editor = this.editor
-            window.$commands = this.editor.commands
-        },
+      window.$editor = this.editor
+      window.$commands = this.editor.commands
+      this.$emit('on-ready')
+    },
 
-        unmounted() {
-            this.editor && this.editor.destroy()
-        },
-    }
+    unmounted() {
+      this.editor && this.editor.destroy()
+    },
+  }
 </script>
