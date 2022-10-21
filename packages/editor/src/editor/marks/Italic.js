f3533c9ac3ec5c618ae464457562e7c40c712488
haner <haner.du@qq.com>
2022-10-21 11:15:45 +0800

    移除：下划线间斜体快捷键

--- packages/editor/src/editor/marks/Italic.js
+++ packages/editor/src/editor/marks/Italic.js
@@ -2,37 +2,37 @@ import Mark from '../lib/Mark'
 import { toggleMark, markInputRule } from '../commands'
 
 export default class Italic extends Mark {
-    get name() {
-        return 'italic'
-    }
+  get name() {
+    return 'italic'
+  }
 
-    get schema() {
-        return {
-            parseDOM: [{ tag: 'i' }, { tag: 'em' }, { style: 'font-style=italic' }],
-            toDOM: () => ['em', 0],
-        }
+  get schema() {
+    return {
+      parseDOM: [{ tag: 'i' }, { tag: 'em' }, { style: 'font-style=italic' }],
+      toDOM: () => ['em', 0],
     }
+  }
 
-    keys({ type }) {
-        return {
-            'Mod-i': toggleMark(type),
-        }
+  keys({ type }) {
+    return {
+      'Mod-i': toggleMark(type),
     }
+  }
 
-    inputRules({ type }) {
-        return [markInputRule(/(?:^|[^_])(_([^_]+)_)$/, type), markInputRule(/(?:^|[^*])(\*([^*]+)\*)$/, type)]
-    }
+  inputRules({ type }) {
+    return [markInputRule(/(?:^|[^*])(\*([^*]+)\*)$/, type)]
+  }
 
-    get toMarkdown() {
-        return {
-            open: '*',
-            close: '*',
-            mixable: true,
-            expelEnclosingWhitespace: true,
-        }
+  get toMarkdown() {
+    return {
+      open: '*',
+      close: '*',
+      mixable: true,
+      expelEnclosingWhitespace: true,
     }
+  }
 
-    parseMarkdown() {
-        return { mark: 'em' }
-    }
+  parseMarkdown() {
+    return { mark: 'em' }
+  }
 }
